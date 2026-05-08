881267acae7e49b4dc9dd4af7223a9144f64161f
logan <40782521+loganoxo@users.noreply.github.com>
2026-05-08 22:28:48 +0800

    anytls自签证书

--- argosbx.sh
+++ argosbx.sh
@@ -99,7 +99,7 @@ if [ -z "$logan_ym" ] || [ ! -f "$logan_private_key" ] || [ ! -f "$logan_cert" ]
     if [ ! -f "$logan_private_key" ] || [ ! -f "$logan_cert" ]; then
         command -v openssl >/dev/null 2>&1 && openssl ecparam -genkey -name prime256v1 -out "$logan_private_key" >/dev/null 2>&1
         command -v openssl >/dev/null 2>&1 && openssl req -new -x509 -days 36500 -key "$logan_private_key" -out "$logan_cert" -subj "/CN=${logan_ym}" >/dev/null 2>&1
-        if [ ! -f "$logan_private_key" ]; then
+        if [ ! -f "$logan_private_key" ] || [ ! -f "$logan_cert" ]; then
             url="https://github.com/loganoxo/argosbx/releases/download/argosbx/private.key"; out="$logan_private_key"; (command -v curl>/dev/null 2>&1 && curl -Ls -o "$out" --retry 2 "$url") || (command -v wget>/dev/null 2>&1 && timeout 3 wget -q -O "$out" --tries=2 "$url")
             url="https://github.com/loganoxo/argosbx/releases/download/argosbx/cert.pem"; out="$logan_cert"; (command -v curl>/dev/null 2>&1 && curl -Ls -o "$out" --retry 2 "$url") || (command -v wget>/dev/null 2>&1 && timeout 3 wget -q -O "$out" --tries=2 "$url")
         fi
